73fbcd0e0bc3e424b6709d3bcb257dbcb68ed657
bunnywong <bunnnywong@gmail.com>
2019-12-20 10:57:19 +0800

    Update conf & styles.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,7 +20,7 @@ module.exports = {
       },
       {
         text: "Github",
-        link: "https://www.github.com/"
+        link: "https://https://github.com/GetToSet/wejh-server-docs/"
       },
       {
         text: "主页",
@@ -52,27 +52,27 @@ module.exports = {
           title: "微精弘 iOS 后端",
           collapsable: true,
           children: [
-            "wejh-ios-server/social-media",
-            "wejh-ios-server/new-media",
-            "wejh-ios-server/traditional-media",
-            "wejh-ios-server/bbs",
-            "wejh-ios-server/blog",
-            "wejh-ios-server/programming",
-            "wejh-ios-server/design",
-            "wejh-ios-server/live",
-            "wejh-ios-server/multimedia",
-            "wejh-ios-server/picture",
-            "wejh-ios-server/anime",
-            "wejh-ios-server/program-update",
-            "wejh-ios-server/university",
-            "wejh-ios-server/forecast",
-            "wejh-ios-server/travel",
-            "wejh-ios-server/shopping",
-            "wejh-ios-server/game",
-            "wejh-ios-server/reading",
-            "wejh-ios-server/government",
-            "wejh-ios-server/study",
-            "wejh-ios-server/other"
+            // "wejh-ios-server/social-media",
+            // "wejh-ios-server/new-media",
+            // "wejh-ios-server/traditional-media",
+            // "wejh-ios-server/bbs",
+            // "wejh-ios-server/blog",
+            // "wejh-ios-server/programming",
+            // "wejh-ios-server/design",
+            // "wejh-ios-server/live",
+            // "wejh-ios-server/multimedia",
+            // "wejh-ios-server/picture",
+            // "wejh-ios-server/anime",
+            // "wejh-ios-server/program-update",
+            // "wejh-ios-server/university",
+            // "wejh-ios-server/forecast",
+            // "wejh-ios-server/travel",
+            // "wejh-ios-server/shopping",
+            // "wejh-ios-server/game",
+            // "wejh-ios-server/reading",
+            // "wejh-ios-server/government",
+            // "wejh-ios-server/study",
+            // "wejh-ios-server/other"
           ]
         }
       ]
