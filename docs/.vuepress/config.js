a03514fe3b973f6ac9aaa199675d93a778acd176
bunnywong <bunnnywong@gmail.com>
2020-01-28 12:15:42 +0800

    Various fixes.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,16 +11,8 @@ module.exports = {
     search: false,
     nav: [
       {
-        text: "链接",
-        link: "/wejh-server/"
-      },
-      {
-        text: "链接",
-        link: "/wejh-ios-server/"
-      },
-      {
         text: "Github",
-        link: "https://https://github.com/GetToSet/wejh-server-docs/"
+        link: "https://github.com/GetToSet/wejh-server-docs/"
       },
       {
         text: "主页",
@@ -56,27 +48,6 @@ module.exports = {
           title: "微精弘 iOS 后端",
           collapsable: true,
           children: [
-            // "wejh-ios-server/social-media",
-            // "wejh-ios-server/new-media",
-            // "wejh-ios-server/traditional-media",
-            // "wejh-ios-server/bbs",
-            // "wejh-ios-server/blog",
-            // "wejh-ios-server/programming",
-            // "wejh-ios-server/design",
-            // "wejh-ios-server/live",
-            // "wejh-ios-server/multimedia",
-            // "wejh-ios-server/picture",
-            // "wejh-ios-server/anime",
-            // "wejh-ios-server/program-update",
-            // "wejh-ios-server/university",
-            // "wejh-ios-server/forecast",
-            // "wejh-ios-server/travel",
-            // "wejh-ios-server/shopping",
-            // "wejh-ios-server/game",
-            // "wejh-ios-server/reading",
-            // "wejh-ios-server/government",
-            // "wejh-ios-server/study",
-            // "wejh-ios-server/other"
           ]
         }
       ]
